112f4b3e0484a0369bdc9cceab75eaedde3b4607
Administrator <632105841@qq.com>
2018-02-09 09:56:00 +0800

    Enchart例子

--- src/views/pollute/care/index.vue
+++ src/views/pollute/care/index.vue
@@ -108,6 +108,7 @@
         <el-button v-else type="primary" @click="update('form')">确 定</el-button>
       </div>
     </el-dialog>
+    <div id="container" style="height: 450px;margin-top: 20px"></div>
   </div>
 </template>
 
@@ -125,7 +126,7 @@
   import {
     mapGetters
   } from 'vuex'
-
+  var echarts = require('echarts')
   export default {
     name: 'care',
     data() {
@@ -426,9 +427,74 @@
     },
     mounted() {
       this.loadAll()
-      // this.restaurants.forEach((value, index, array) => {
-      //   this.restaurants[index]['value'] = value['name']
-      // })
+      var dom = document.getElementById('container')
+      var myChart = echarts.init(dom)
+      var option = null
+      option = {
+        title: {
+          text: '折线图demo'
+        },
+        tooltip: {
+          trigger: 'axis'
+        },
+        legend: {
+          data: ['邮件营销', '联盟广告', '视频广告', '直接访问', '搜索引擎']
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        toolbox: {
+          feature: {
+            saveAsImage: {}
+          }
+        },
+        xAxis: {
+          type: 'category',
+          boundaryGap: false,
+          data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+        },
+        yAxis: {
+          type: 'value'
+        },
+        series: [
+          {
+            name: '邮件营销',
+            type: 'line',
+            stack: '总量',
+            data: [120, 132, 101, 134, 90, 230, 210]
+          },
+          {
+            name: '联盟广告',
+            type: 'line',
+            stack: '总量',
+            data: [220, 182, 191, 234, 290, 330, 310]
+          },
+          {
+            name: '视频广告',
+            type: 'line',
+            stack: '总量',
+            data: [150, 232, 201, 154, 190, 330, 410]
+          },
+          {
+            name: '直接访问',
+            type: 'line',
+            stack: '总量',
+            data: [320, 332, 301, 334, 390, 330, 320]
+          },
+          {
+            name: '搜索引擎',
+            type: 'line',
+            stack: '总量',
+            data: [820, 932, 901, 934, 1290, 1330, 1320]
+          }
+        ]
+      }
+      if (option && typeof option === 'object') {
+        myChart.setOption(option, true)
+      }
     }
   }
 </script>
